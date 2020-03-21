301a2acf02440ccbc328f3d6c3cb6d5b7961d4de
Santosh <balijasantosh@gmail.com>
2020-03-21 15:26:08 -0500

    My first commit

---
+++ LR_w_s.py
@@ -0,0 +1,189 @@
+import os
+import string
+import numpy as np
+
+def load_file_paths(path):
+    
+    file_paths = []
+    for root, dirs, file in os.walk(path):
+        for f in file:
+            if '.txt' in f:
+                file_paths.append(os.path.join(root, f))
+    return file_paths
+
+
+# Get all the words from file_paths
+def all_words(File_paths):
+    
+    list_of_words = []
+    for file in File_paths:
+        f = open(file,'r',errors='ignore')
+        words = f.read().replace('\n',' ')
+        list_of_words.extend(words.split())
+    list_of_words = [''.join(c for c in s if c not in string.punctuation) for s in list_of_words]
+    list_of_words = [x for x in list_of_words if x]
+    return list_of_words
+    
+
+def Sigmoid(x):
+    sig = 1.0/(1 + np.exp(-x))
+    return sig
+
+def update_weights(l,iterations, weights, words, true_labels):
+                     
+        for i in range(iterations): 
+            p = np.dot(words,weights)            
+            t = np.array(p,dtype=np.float32)
+            s = Sigmoid(t)
+            labels = true_labels
+            a = np.dot(words.transpose(),labels - s)  
+            weights = weights + (0.001*(a))-(0.001*l*weights)  
+            
+        return weights
+
+def LR(test_data, label, weights):
+    y = np.array(np.dot(test_data,weights),dtype=np.float32)
+    c_h =0
+    c_s = 0
+    in_h = 0
+    in_s = 0
+    p_labels=[]
+  
+    for i in y:
+        if i>0:
+            p_labels.append(1)
+        else:
+            p_labels.append(0)
+    
+    for i in range(len(label)):
+        if label[i] == p_labels[i]:
+            if label[i] == 0:
+                c_s = c_s + 1
+            else:
+                c_h = c_h + 1
+        else:        
+            if p_labels[i] == 0:
+                in_s = in_s + 1
+            else:
+                in_h = in_h + 1
+                
+    
+    return c_h,c_s,in_h,in_s
+
+if __name__=='__main__':
+    
+    # file paths for both ham and spam train fils
+    ham_train_files = load_file_paths('assignment3_train/train/ham')
+    spam_train_files = load_file_paths('assignment3_train/train/spam')
+    
+    all_train = ham_train_files + spam_train_files
+    #Words in Ham and Spam train files 
+    ham_train_words  =  all_words(ham_train_files)
+    spam_train_words = all_words(spam_train_files)
+    # combined train words in ham and spam
+    words_combined = ham_train_words + spam_train_words
+    # unique words in words_combined 
+    words_combined_unique = list(dict.fromkeys(words_combined)) 
+    
+
+    
+    c =0;
+    train_dict = dict()  
+    
+    for file in all_train:
+        all_words_train = []
+        f = open(file,'r',errors ='ignore')     
+        words = f.read().replace('\n',' ')
+        all_words_train.extend(words.split())
+        all_words_train = [''.join(c for c in s if c not in string.punctuation) for s in all_words_train]   
+        all_words_train = [x for x in all_words_train if x]
+        
+        r = [0]*len(words_combined_unique)
+        for i in range(len(words_combined_unique)):        
+            if words_combined_unique[i] in all_words_train:
+                feature_count = all_words_train.count(words_combined_unique[i])
+                r[i] = feature_count               
+            else:
+                r[i] = 0
+        r.insert(0,1)                
+        train_dict[c]=r
+        c = c + 1
+    
+        
+
+    
+    labels = []             
+    for i in range(len(ham_train_files)):
+        labels.append(1)
+        
+    for i in range(len(spam_train_files)):
+        labels.append(0)
+    
+    labels_np = np.asarray(labels)
+    
+    
+    words_each_file = list(train_dict.values())
+    
+    words_each_file_np = np.asarray(words_each_file)
+ 
+    ham_test_files = load_file_paths('assignment3_test/test/ham_test')
+    spam_test_files = load_file_paths('assignment3_test/test/spam_test')
+    
+    all_test = ham_test_files + spam_test_files
+    
+    test_dict = dict()
+    d = 0
+    for file in all_test:
+        all_words_test = []
+        f = open(file,'r',errors ='ignore')     
+        wordstest = f.read().replace('\n',' ')
+        all_words_test.extend(wordstest.split())
+        all_words_test = [''.join(c for c in s if c not in string.punctuation) for s in  all_words_test]   
+        all_words_test = [x for x in all_words_test if x]
+        q = [0]*len(words_combined_unique)
+        for i in range(len(words_combined_unique)):        
+            if words_combined_unique[i] in all_words_test:
+                feature_count = all_words_test.count(words_combined_unique[i])
+                q[i]=feature_count               
+            else:
+                q[i]=0
+                
+        q.insert(0,1)                
+        test_dict[d]=q
+        d = d + 1
+    
+    labels_1 = []             
+    for i in range(len(ham_test_files)):
+        labels_1.append(1)
+    for i in range(len(spam_test_files)):
+        labels_1.append(0)
+    
+    words_each_file_t = list(test_dict.values())
+    
+    words_each_file_t_np = np.asarray(words_each_file_t)
+    
+    lamda = [0.05,0.09,0.1]
+    
+    iterations = 100
+    
+    for l in lamda:
+        weights = np.zeros(words_each_file_np.shape[1])
+
+        new_weights = update_weights(l,iterations,weights,words_each_file_np,labels_np)      
+   
+        c_p_ham, c_p_spam, in_p_ham, in_p_spam = LR( words_each_file_t_np, labels_1, new_weights )
+    
+        ham_accuracy = c_p_ham/(c_p_ham + in_p_ham)
+    
+        spam_accuracy = c_p_spam/(c_p_spam+in_p_spam)
+    
+        total_accuracy = (c_p_ham + c_p_spam)/(c_p_ham + in_p_ham + c_p_spam + in_p_spam)
+    
+        print("HamAccuracy :",ham_accuracy*100)
+    
+        print("SpamAccuracy :",spam_accuracy*100)
+        
+        print("TotalAccuracy :",total_accuracy*100)
+        
+        print("\n")
+    
---
+++ NB_w_s.py
@@ -0,0 +1,138 @@
+import os
+import string
+import math
+
+
+# Appending the file paths in a list   
+def load_file_paths(path):
+    
+    file_paths = []
+    for root, dirs, file in os.walk(path):
+        for f in file:
+            if '.txt' in f:
+                file_paths.append(os.path.join(root, f))
+    return file_paths
+
+# Get all the words from file_paths
+def all_words(File_paths):
+    #print('getWords')
+    list_of_words = []
+    for file in File_paths:
+        f = open(file,'r',errors='ignore')
+        words = f.read().replace('\n',' ')
+        list_of_words.extend(words.split())
+    list_of_words = [''.join(c for c in s if c not in string.punctuation) for s in list_of_words]
+    list_of_words = [x for x in list_of_words if x]
+    return list_of_words
+    
+
+def count(words):
+    d = {}
+    for i in words:
+        if i in d:
+            d[i] += 1
+        else:
+            d[i] = 1
+    return d
+        
+
+def conditional_prob(words,label,words_combined_unique):     
+    conditional_prob_final = {} 
+    b = len(words_combined_unique)                      
+    DictWords = count(words)
+    for w in words_combined_unique:
+        if w in DictWords:
+            conditional_prob_final[w+':'+label] =  float((DictWords[w] + 1)/(len(words) + b)) 
+        else:
+            conditional_prob_final[w+':'+label] =  float(1/(len(words) + b))   
+    return conditional_prob_final
+
+def NB(prior,file_paths_test,cond_prob,true_label):
+    c_p = 0;
+    in_p = 0;
+    for file in file_paths_test:
+        words= []
+        f = open(file,'r',errors ='ignore')     
+        w = f.read().replace('\n',' ')
+        words.extend(w.split())
+        words = [''.join(c for c in s if c not in string.punctuation) for s in words]   
+        words = [x for x in words if x]
+        
+        words_unique = count(words)
+        
+        label = ['ham','spam']
+        predval = {}
+        for lb in label:
+            if (lb == 'ham'):
+                pr = prior[0]
+            else:
+                pr = prior[1]
+            sumprob = 0
+
+            for w,c in words_unique.items():
+                if (w+':'+lb) in cond_prob:
+                    sumprob += c*(math.log(cond_prob[w+':'+lb]))
+            predval[lb] = sumprob + math.log(pr) 
+
+        max_value = max(predval.values()) 
+        predlabel = (([k for k, v in predval.items() if v == max_value]))
+        if (predlabel[0]==true_label):
+            c_p = c_p + 1
+        else:
+            in_p = in_p + 1
+    return c_p, in_p
+            
+
+if __name__=='__main__':
+    # file paths for both ham and spam train fils
+    ham_train_files = load_file_paths('assignment3_train/train/ham')
+    spam_train_files = load_file_paths('assignment3_train/train/spam')
+    #Words in Ham and Spam train files 
+    ham_train_words  =  all_words(ham_train_files)
+    spam_train_words = all_words(spam_train_files)
+    # combined train words in ham and spam
+    words_combined = ham_train_words + spam_train_words
+    # unique words in words_combined 
+    words_combined_unique = list(dict.fromkeys(words_combined))  
+    
+
+    ham_conditional_p = dict()
+    spam_conditional_p = dict()
+    # conditional probabilites of ham and spam train words
+    ham_conditional_p = conditional_prob(ham_train_words,'ham',words_combined_unique)  
+    spam_conditional_p = conditional_prob(spam_train_words,'spam',words_combined_unique)  
+   
+    
+    # combined conditional probabilities
+    combined_conditional = {**ham_conditional_p,**spam_conditional_p}    
+    
+   
+    # priors of ham and spam
+    prior_ham = len(ham_train_files)/(len(ham_train_files)+len(spam_train_files))
+    prior_spam = len(spam_train_files)/(len(ham_train_files)+len(spam_train_files))
+    prior = [prior_ham, prior_spam]
+    
+    ham_test_files = load_file_paths('assignment3_test/test/ham_test')
+    spam_test_files = load_file_paths('assignment3_test/test/spam_test')
+
+    c_p_ham, in_p_ham = NB(prior,ham_test_files,combined_conditional,'ham') 
+    # ham accuracy
+    ham_accuracy = c_p_ham/(c_p_ham + in_p_ham)
+    
+    c_p_spam, in_p_spam = NB(prior,spam_test_files,combined_conditional,'spam') 
+    
+    # spam_accuracy
+    spam_accuracy = c_p_spam/(c_p_spam+in_p_spam)
+    # total _accuracy
+    total_accuracy = (c_p_ham + c_p_spam)/(c_p_ham + in_p_ham + c_p_spam + in_p_spam)
+    print("HamAccuracy:",ham_accuracy*100)
+    print("SpamAccuracy:",spam_accuracy*100)
+    print("TotalAccuracy:",total_accuracy*100) 
+    
+    
+    
+    
+    
+    
+    
+     
